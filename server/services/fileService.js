f7e92949c7ee8dbeb8afa3416d92736b1c1381be
Vladislav <vladway827@gmail.com>
2023-03-15 13:22:09 +0300

    added new func uload files

--- server/services/fileService.js
+++ server/services/fileService.js
@@ -5,17 +5,15 @@ const config = require("config");
 class FileService {
   createDir(file) {
     const filePath = `${config.get("filePath")}\\${file.user}\\${file.path}`;
-    console.log('filePath', fs.mkdirSync(filePath))
     return new Promise((resolve, reject) => {
       try {
-        // TODO разобраться почему не создается файл
         if (!fs.existsSync(filePath)) {
-          fs.mkdtempSync(filePath);
+          fs.mkdirSync(filePath);
           return resolve({ message: "File was created" });
         } else {
           return reject({ message: "File already exist" });
         }
-      } catch (error) {
+      } catch (e) {
         return reject({ message: "File error" });
       }
     });
