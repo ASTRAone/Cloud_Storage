d8693019f6a36dba0d9eb101635bd0e58fdad9c8
Vladislav <vladway827@gmail.com>
2023-03-15 13:20:54 +0300

    fixed error upload files

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -6,9 +6,8 @@ class FileController {
   async createDir(req, res) {
     try {
       const { name, type, parent } = req.body;
-      const file = new File({ name, type, parent, user: user.id });
+      const file = new File({ name, type, parent, user: req.user.id });
       const parentFile = await File.findOne({ _id: parent });
-
       if (!parentFile) {
         file.path = name;
         await fileService.createDir(file);
@@ -18,11 +17,24 @@ class FileController {
         parentFile.childs.push(file._id);
         await parentFile.save();
       }
-      await parentFile.save();
+      await file.save();
       return res.json(file);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json(e);
+    }
+  }
+
+  async getFiles(req, res) {
+    try {
+      const files = await File.find({
+        user: req.user.id,
+        parent: req.query.parent,
+      });
+      return res.json({ files });
     } catch (error) {
       console.log(error);
-      return res.status(400).json(error);
+      return res.status(500).json({ message: "Can not get files" });
     }
   }
 }
