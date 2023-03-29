af4ef625a337e24b821da5c51d1be242470861a0
Vladislav <vladway827@gmail.com>
2023-03-30 01:21:38 +0300

    fix http

--- client/src/http/http.ts
+++ client/src/http/http.ts
@@ -1,7 +1,10 @@
+// import { useNavigate } from 'react-router-dom';
+
 import axios from 'axios';
 import { AUTH_HEADER } from '@utils/headers';
+// import { LOGIN_ROUTE } from '@utils/contants';
 
-import { AuthApi } from '@api/AuthApi';
+// import { AuthApi } from '@api/AuthApi';
 
 export const $api = axios.create({
   withCredentials: true,
@@ -21,25 +24,31 @@ $api.interceptors.request.use(
   },
 );
 
+// TODO вынести все отдельно
 $api.interceptors.response.use(
   (res) => {
     return res;
   },
   async (err) => {
     const originalConfig = err.config;
+    // const navigate = useNavigate();
+    console.log('originalConfig', originalConfig);
+    console.log('err.response', err.response);
 
     if (err.response) {
       // Access Token was expired
-      if (err.response.status === 401 && !originalConfig._retry) {
+      if (err.response.status === 401) {
         originalConfig._retry = true;
 
         try {
-          const rs = await AuthApi.refresh();
-          const { accessToken } = rs.data;
-          window.localStorage.setItem(AUTH_HEADER, accessToken);
-          $api.defaults.headers.common[AUTH_HEADER] = accessToken;
+          // const rs = await AuthApi.refresh();
+          // const { accessToken } = rs.data;
+          // localStorage.setItem(AUTH_HEADER, `Bearer ${accessToken}`);
+          // $api.defaults.headers.common[AUTH_HEADER] = `Bearer ${accessToken}`;
+          localStorage.removeItem(AUTH_HEADER);
+          // navigate(LOGIN_ROUTE);
 
-          return $api(originalConfig);
+          // return $api(originalConfig);
         } catch (_error: any) {
           if (_error.response && _error.response.data) {
             return Promise.reject(_error.response.data);
